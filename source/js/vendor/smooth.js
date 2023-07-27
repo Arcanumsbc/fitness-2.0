f52578f41fd5e35ef91466189a27cd909a364d34
Arcanumsbc <arcanumsbc@yandex.ru>
2023-07-27 15:09:49 +0300

    fix bags chrome header

--- source/js/vendor/smooth.js
+++ source/js/vendor/smooth.js
@@ -1,15 +1,15 @@
 const initSmoothNavigation = (elem) => {
-  const blockId = elem.getAttribute("href");
-  document.querySelector("" + blockId).scrollIntoView({
-    behavior: "smooth",
-    block: "start",
+  const blockId = elem.getAttribute('href');
+  document.querySelector('' + blockId).scrollIntoView({
+    behavior: 'smooth',
+    block: 'start',
   });
 };
 
-const headerButton = document.querySelector(".main-header__content a");
-const buttonAbonement = document.querySelectorAll(".season-tickets__button");
+const headerButton = document.querySelector('.main-header__content a');
+const buttonAbonement = document.querySelectorAll('.season-tickets__button');
 
-headerButton.addEventListener("click", (evt) => {
+headerButton.addEventListener('click', (evt) => {
   evt.preventDefault();
   initSmoothNavigation(headerButton);
   setTimeout(() => {
@@ -17,7 +17,7 @@ headerButton.addEventListener("click", (evt) => {
   }, 700);
 });
 
-const navigationItem = document.querySelectorAll(".main-footer__list a");
+const navigationItem = document.querySelectorAll('.main-footer__list a');
 
 navigationItem.forEach((item) => {
   item.addEventListener("click", (evt) => {
@@ -26,9 +26,9 @@ navigationItem.forEach((item) => {
   });
 });
 
-const footerLogo = document.querySelector(".main-footer__logo a");
+const footerLogo = document.querySelector('.main-footer__logo a');
 
-footerLogo.addEventListener("click", (evt) => {
+footerLogo.addEventListener('click', (evt) => {
   evt.preventDefault(evt);
   initSmoothNavigation(footerLogo);
 });
